d4b01007bc6cd9509232dc4de4465b6c93287570
harlembeibs <harlembeibs@yandex.ru>
2022-09-01 00:37:44 +0300

    m

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayHello(name) {
+    // Преобразуем входные данные
+  let greeting = 'Привет, ' + name;
+    // Возвращаем результат
+  return greeting;    
+}
+
+
+console.log(sayHello('Алиса')); // "Привет, Алиса" 
